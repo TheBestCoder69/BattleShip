e7d2ada8245ca4c8b42962cac71895fc205b268b
Alec-compsci <alexanderch0621@gmail.com>
2024-10-29 16:06:42 +0000

    working so far

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ shipY = np.random.randint(0, 5)
 # Places ship in a random area in the "ocean"
 board[shipX, shipY] = 1
 
+#User input for target location
+x = int(input("x point of target"))
+y = int(input("y point of target"))
+
 def tableSetup():
     print(board)
 
 tableSetup()
+
+if (board[x,y]==1):
+    print("HIT")
+else:
+    print("Miss")
